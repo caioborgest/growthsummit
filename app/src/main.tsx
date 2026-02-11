15c7d144a0c01e1bbcab1955e07e77d6295b8154
thalitameloadv <company.cbx@gmail.com>
2026-02-11 19:48:18 -0300

    2

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { HelmetProvider } from 'react-helmet-async';
 import { QueryProvider } from '@/api/providers/QueryProvider';
 import { AuthProvider } from '@/contexts/AuthContext';
 import { ProjectProvider } from '@/contexts/ProjectContext';
@@ -13,22 +14,24 @@ validateConfig();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <QueryProvider>
-      <AuthProvider>
-        <ProjectProvider>
-          <App />
-          <Toaster 
-            position="top-right"
-            toastOptions={{
-              style: {
-                background: '#1E293B',
-                border: '1px solid #334155',
-                color: '#fff',
-              },
-            }}
-          />
-        </ProjectProvider>
-      </AuthProvider>
-    </QueryProvider>
+    <HelmetProvider>
+      <QueryProvider>
+        <AuthProvider>
+          <ProjectProvider>
+            <App />
+            <Toaster
+              position="top-right"
+              toastOptions={{
+                style: {
+                  background: '#1E293B',
+                  border: '1px solid #334155',
+                  color: '#fff',
+                },
+              }}
+            />
+          </ProjectProvider>
+        </AuthProvider>
+      </QueryProvider>
+    </HelmetProvider>
   </StrictMode>
 );
