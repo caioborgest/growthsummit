322ab1022f8d6384ea51a4f120a33616e3eac2fa
thalitameloadv <caioborgest@gmail.com>
2026-02-11 21:20:04 -0300

    1

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,6 +1,5 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { HelmetProvider } from 'react-helmet-async';
 import { QueryProvider } from '@/api/providers/QueryProvider';
 import { AuthProvider } from '@/contexts/AuthContext';
 import { ProjectProvider } from '@/contexts/ProjectContext';
@@ -14,24 +13,22 @@ validateConfig();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <HelmetProvider>
-      <QueryProvider>
-        <AuthProvider>
-          <ProjectProvider>
-            <App />
-            <Toaster
-              position="top-right"
-              toastOptions={{
-                style: {
-                  background: '#1E293B',
-                  border: '1px solid #334155',
-                  color: '#fff',
-                },
-              }}
-            />
-          </ProjectProvider>
-        </AuthProvider>
-      </QueryProvider>
-    </HelmetProvider>
+    <QueryProvider>
+      <AuthProvider>
+        <ProjectProvider>
+          <App />
+          <Toaster
+            position="top-right"
+            toastOptions={{
+              style: {
+                background: '#1E293B',
+                border: '1px solid #334155',
+                color: '#fff',
+              },
+            }}
+          />
+        </ProjectProvider>
+      </AuthProvider>
+    </QueryProvider>
   </StrictMode>
 );
