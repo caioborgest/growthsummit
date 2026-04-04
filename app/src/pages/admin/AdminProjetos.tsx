da7e1d3663f711ccbbd7807df62605d6ac885e82
caioborges <caioborgest@gmail.com>
2026-04-04 15:22:47 -0300

    1

--- app/src/pages/admin/AdminProjetos.tsx
+++ app/src/pages/admin/AdminProjetos.tsx
@@ -1129,6 +1129,7 @@ export default function AdminProjetos() {
                     </div>
                   </div>
                 </TabsContent>
+                </div>
               </Tabs>
 
 
