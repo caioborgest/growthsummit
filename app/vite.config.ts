47b47f9a4903fd52d70052f134ff569fc86f054a
caioborges <caioborgest@gmail.com>
2026-04-05 12:30:39 -0300

    Update vite.config.ts

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -5,6 +5,9 @@ import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  define: {
+    __BUILD_TIME__: JSON.stringify(new Date().toISOString()),
+  },
   plugins: [
     react(),
     VitePWA({
